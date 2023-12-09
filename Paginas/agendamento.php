fa1bb8a7786dd35bcd9b05e17210b8f28e1acf9f
Jaime-junio <jaimejunio629@gmail.com>
2023-12-09 11:07:49 -0300

    ..

--- Paginas/agendamento.php
+++ Paginas/agendamento.php
@@ -1,28 +1,5 @@
 
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agendamento</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <h1>Agendamento</h1>
-    <form action="index.php" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" name="nome"><br><br>
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" name="email"><br><br>
-        <label for="telefone">Telefone:</label>
-        <input type="tel" id="telefone" name="telefone"><br><br>
-        <label for="data">Data:</label>
-        <input type="date" id="data" name="data"><br><br>
-        <label for="hora">Hora:</label>
-        <input type="time" id="hora" name="hora"><br><br>
-        <input type="submit" value="Agendar">
-    </form>
-   <?php 
+<?php 
         // Verificar se o formulário foi enviado
         if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['telefone']) && isset($_POST['data']) && isset($_POST['hora'])) {
             $nome = $_POST['nome'];
@@ -53,5 +30,3 @@
             $conn->close();
         }
     ?>
-</body>
-</html>
